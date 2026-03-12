858962d3281e77bca9a652cda0acb0528f0bdbd8
Gerardo! <pliegolugohectorgerardo@gmail.com>
2026-03-11 20:19:52 -0600

    Please provide the file changes to generate a commit message.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://GerardoPz.github.io',
+  base: '/ingeniero-electronico',
+
   vite: {
     plugins: [tailwindcss()],
   },
